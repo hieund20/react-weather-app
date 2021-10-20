fd5418189091bd653be529ec78c39999ccbdbe11
hieund20 <hieund898@gmail.com>
2021-10-20 09:45:18 +0700

    fix bug deploy

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 import moment from 'moment';
