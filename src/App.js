60b168743735c7c0666ddf0fd8f33f21c6727ca8
hieund20 <hieund898@gmail.com>
2021-10-20 09:42:49 +0700

    fix bug eslint

--- src/App.js
+++ src/App.js
@@ -111,6 +111,7 @@ function App() {
 
   useEffect((tempState) => {
     handleConvertTemperature(tempState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [temperatureState]);
 
 
